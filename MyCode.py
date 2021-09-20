a4eac926a7c668ea42b0a303ca41e8cafaa3b2b4
Vishal Chhipa <72239915+Vishal1029@users.noreply.github.com>
2021-09-20 13:26:30 +0530

    Add files via upload

---
+++ MyCode.py
@@ -0,0 +1,9 @@
+#TOPIC = input from user
+x = int(input("Enter 1st number"))
+y = int(input("Enter 1st number"))
+z = x + y
+print(z)
+ch = input('Enter a char')[0]
+print(ch[0])
+result = eval(input('Enter a expr'))
+print(result)
